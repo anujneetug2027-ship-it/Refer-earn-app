966a23c141145b14e930b3078a28e951877e231f
AmbikaShelf <anujneetug2027@gmail.com>
2026-03-09 22:55:36 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,18 +2,20 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: String,
-    email: { type: String, unique: true },
-      referralCode: { type: String, unique: true },
-        referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-          rewardBalance: { type: Number, default: 0 },
-            otp: String,
-              otpExpires: Date
-              });
-walletTransactions: [{
-  paymentId: String,
-  orderId:   String,
-  amount:    Number,
-  date:      { type: Date, default: Date.now },
-  type:      { type: String, default: 'credit' }
-}]
-              module.exports = mongoose.model('User', userSchema);
+  email: { type: String, unique: true },
+  referralCode: { type: String, unique: true },
+  referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
+  rewardBalance: { type: Number, default: 0 },
+  otp: String,
+  otpExpires: Date,
+
+  walletTransactions: [{
+    paymentId: String,
+    orderId: String,
+    amount: Number,
+    date: { type: Date, default: Date.now },
+    type: { type: String, default: 'credit' }
+  }]
+});
+
+module.exports = mongoose.model('User', userSchema);
